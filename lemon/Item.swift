fc305b2550ac18d68e9e1277b74a1b76cff5adb0
Dmitriy Pervin <theforner@gmail.com>
2025-08-21 00:14:11 +0500

    Initial Commit

---
+++ lemon/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  lemon
+//
+//  Created by Dmitrii Pervin on 21.08.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
